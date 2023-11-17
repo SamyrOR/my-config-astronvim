d666a7856c2c26ca5dfe227e17b604d9643c1ab5
Samyr Ribeiro <saamyr@live.com>
2023-11-17 11:10:47 -0300

    chore: made neo-tree show hidden files

---
+++ plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+    opts = function()
+      return {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+          }
+        }
+      }
+    end
+}
